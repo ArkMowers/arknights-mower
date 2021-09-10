cef1396510de2420998a685dcaa6aee3c5628cce
Konano <nanoapezlk@gmail.com>
2021-09-11 02:31:25 +0800

    fix(matcher): remove cut, add scope

--- utils/matcher.py
+++ utils/matcher.py
@@ -17,12 +17,25 @@ class FlannBasedMatcher():
         self.kp, self.des = SIFT.detectAndCompute(origin, None)
         logger.debug(f'FlannBasedMatcher init: shape ({origin.shape})')
 
-    def match(self, query, ret_square=True, draw=False):
+    def match(self, query, ret_square=True, draw=False, scope=None):
         if self.des is None:
             logger.debug('feature points is None')
             if ret_square:
                 return None
             return False
+
+        if scope is not None:
+            logger.debug(f'before: {len(self.kp)}')
+            logger.debug(f'scope: {scope}')
+            kp0, des0 = [], []
+            for kp, des in zip(self.kp, self.des):
+                if scope[0][0] <= kp.pt[0] and scope[0][1] <= kp.pt[1] and kp.pt[0] <= scope[1][0] and kp.pt[1] <= scope[1][1]:
+                    kp0.append(kp)
+                    des0.append(des)
+            logger.debug(f'after: {len(kp0)}')
+            kp0, des0 = np.array(kp0), np.array(des0)
+        else:
+            kp0, des0 = self.kp, self.des
 
         h, w = query.shape
         kp, des = SIFT.detectAndCompute(query, None)
@@ -30,7 +43,7 @@ class FlannBasedMatcher():
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
         flann = cv2.FlannBasedMatcher(index_params, search_params)
-        matches = flann.knnMatch(des, self.des, k=2)
+        matches = flann.knnMatch(des, des0, k=2)
 
         """store all the good matches as per Lowe's ratio test."""
         good = []
@@ -38,10 +51,9 @@ class FlannBasedMatcher():
             if x.distance < GOOD_DISTANCE_LIMIT * y.distance:
                 good.append(x)
 
-        good_kp = [kp[x.queryIdx].pt for x in good]
-        if len(good_kp):
-            good_area_rate = np.ptp(
-                [x[0] for x in good_kp]) * np.ptp([x[1] for x in good_kp]) / h / w
+        good_kp_x = [kp[x.queryIdx].pt[0] for x in good]
+        if len(good_kp_x):
+            good_area_rate = np.ptp(good_kp_x) / w
         else:
             good_area_rate = 0
 
@@ -50,7 +62,7 @@ class FlannBasedMatcher():
 
         if draw:
             result = cv2.drawMatches(
-                query, kp, self.origin, self.kp, good, None)
+                query, kp, self.origin, kp0, good, None)
             plt.imshow(result, 'gray')
             plt.show()
 
@@ -64,7 +76,7 @@ class FlannBasedMatcher():
         src_pts = np.float32(
             [kp[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
         dst_pts = np.float32(
-            [self.kp[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+            [kp0[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
         """calculated transformation matrix and the mask"""
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
@@ -86,11 +98,14 @@ class FlannBasedMatcher():
                 return None
             return False
 
-        if good_area_rate < 0.3:
+        if good_area_rate < 0.5:
             logger.debug('good_area_rate is not enough')
             if ret_square:
                 return None
             return False
+
+        logger.info(
+            f'matches: {len(good)} / {len(matches)} / {len(des)} / {len(good) / len(des)} / {good_area_rate}')
 
         """draw the result"""
         if draw or MATCHER_DEBUG:
@@ -99,7 +114,7 @@ class FlannBasedMatcher():
             draw_params = dict(matchColor=(
                 0, 255, 0), singlePointColor=None, matchesMask=matchesMask, flags=2)
             result = cv2.drawMatches(
-                query, kp, origin, self.kp, good, None, **draw_params)
+                query, kp, origin, kp0, good, None, **draw_params)
             plt.imshow(result, 'gray')
             plt.show()
 
