17a23cfdc2a709ce2284cb6e4946cc5e01f35ead
Konano <nanoapezlk@gmail.com>
2021-10-09 04:25:46 +0800

    feat: update diy

--- diy.py
+++ diy.py
@@ -1,5 +1,31 @@
+from pathlib import Path
+from arknights_mower.utils import detector
 from arknights_mower.strategy import Solver
+from arknights_mower.utils.log import logger, init_fhlr
+from arknights_mower.utils.adb import ADBConnector
+from arknights_mower.utils.recognize import Recognizer, threshole, bytes2img
+from arknights_mower.utils import config
+from matplotlib import pyplot as plt
 
-cli = Solver()
 
-cli.mission()
+def debuglog():
+    logger.handlers[0].setLevel('DEBUG')
+
+
+def simulate():
+    config.LOGFILE_PATH = './log'
+    config.SCREENSHOT_PATH = './screenshot'
+    config.SCREENSHOT_MAXNUM = 50
+    init_fhlr()
+    cli = Solver()
+    cli.base()
+    cli.credit()
+    cli.fight() 
+    cli.shop() 
+    cli.recruit() 
+    cli.mission() 
+    cli.mail()
+
+
+debuglog()
+simulate()
