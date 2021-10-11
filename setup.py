139b30b37ba558a09e0cc60f4f1a02adb2c1fe92
Konano <nanoapezlk@gmail.com>
2021-10-11 21:25:25 +0800

    fix: update requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/Konano/arknights-mower',
     packages=setuptools.find_packages(),
-    install_requires=['colorlog', 'opencv_python', 'matplotlib', 'numpy', 'scikit_image', 'sklearn'],
+    install_requires=['colorlog', 'opencv_python', 'matplotlib',
+                      'numpy', 'scikit_image', 'sklearn', 'onnxruntime', 'pyclipper', 'shapely'],
     include_package_data=True,
     entry_points={'console_scripts': [
         'arknights-mower=arknights_mower.__main__:main']},
