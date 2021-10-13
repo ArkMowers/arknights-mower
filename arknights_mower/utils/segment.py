ca9d3bc7e965c2939eab3b61016c0d56abff2978
Konano <nanoapezlk@gmail.com>
2021-10-13 19:16:33 +0800

    feat: add segment.base

--- arknights_mower/utils/segment.py
+++ arknights_mower/utils/segment.py
@@ -7,6 +7,12 @@ from .recognize import RecognizeError
 from .log import logger
 
 
+def get_poly(x1, x2, y1, y2):
+    x1, x2 = int(x1), int(x2)
+    y1, y2 = int(y1), int(y2)
+    return np.array([[x1, y1], [x1, y2], [x2, y2], [x2, y1]])
+
+
 def credit(im, draw=False):
     """
     信用交易所特供的图像分割算法
@@ -60,19 +66,19 @@ def credit(im, draw=False):
         while ptp(left) < 50:
             left += 1
 
-        split_x = [up, (up + down) // 2, down]
-        split_y = [left] + [left + (right - left) //
+        split_x = [left] + [left + (right - left) //
                             5 * i for i in range(1, 5)] + [right]
+        split_y = [up, (up + down) // 2, down]
 
         ret = []
         for x1, x2 in zip(split_x[:-1], split_x[1:]):
             for y1, y2 in zip(split_y[:-1], split_y[1:]):
-                ret.append(((y1, x1), (y2, x2)))
+                ret.append(((x1, y1), (x2, y2)))
 
         if draw:
             for x1, x2 in zip(split_x[:-1], split_x[1:]):
                 for y1, y2 in zip(split_y[:-1], split_y[1:]):
-                    cv2.polylines(im, [np.array([[y1, x1], [y1, x2], [y2, x2], [y2, x1]])],
+                    cv2.polylines(im, [get_poly(x1, x2, y1, y2)],
                                   True, 0, 10, cv2.LINE_AA)
             plt.imshow(im)
             plt.show()
@@ -148,19 +154,102 @@ def recruit(im, draw=False):
         while adj_y(right) < 50:
             right -= 1
 
-        split_x = [up, (up + down) // 2, down]
-        split_y = [left, (left + right) // 2, right]
+        split_x = [left, (left + right) // 2, right]
+        split_y = [up, (up + down) // 2, down]
 
         ret = []
         for x1, x2 in zip(split_x[:-1], split_x[1:]):
             for y1, y2 in zip(split_y[:-1], split_y[1:]):
-                ret.append(((y1, x1), (y2, x2)))
+                ret.append(((x1, y1), (x2, y2)))
 
         if draw:
             for x1, x2 in zip(split_x[:-1], split_x[1:]):
                 for y1, y2 in zip(split_y[:-1], split_y[1:]):
-                    cv2.polylines(im, [np.array(
-                        [[y1, x1], [y1, x2], [y2, x2], [y2, x1]])], True, 0, 10, cv2.LINE_AA)
+                    cv2.polylines(im, [get_poly(x1, x2, y1, y2)],
+                                  True, 0, 10, cv2.LINE_AA)
+            plt.imshow(im)
+            plt.show()
+
+        logger.debug(f'segment.recruit: {ret}')
+        return ret
+
+    except Exception as e:
+        logger.debug(traceback.format_exc())
+        raise RecognizeError
+
+
+def base(im, central, draw=False):
+    """
+    基建布局的图像分割算法
+    """
+    try:
+        ret = {}
+
+        x1, y1 = central[0]
+        x2, y2 = central[1]
+        alpha = (y2 - y1) / 160
+        x1 -= 170 * alpha
+        x2 += 182 * alpha
+        y1 -= 67 * alpha
+        y2 += 67 * alpha
+        central = get_poly(x1, x2, y1, y2)
+        ret['central'] = central
+
+        for i in range(1, 5):
+            y1 = y2 + 25 * alpha
+            y2 = y1 + 134 * alpha
+            if i & 1:
+                dormitory = get_poly(x1, x2 - 158 * alpha, y1, y2)
+            else:
+                dormitory = get_poly(x1 + 158 * alpha, x2, y1, y2)
+            ret[f'dormitory_{i}'] = dormitory
+
+        x1, y1 = ret['dormitory_1'][0]
+        x2, y2 = ret['dormitory_1'][2]
+
+        x1 = x2 + 419 * alpha
+        x2 = x1 + 297 * alpha
+        factory = get_poly(x1, x2, y1, y2)
+        ret[f'factory'] = factory
+
+        y2 = y1 - 25 * alpha
+        y1 = y2 - 134 * alpha
+        meeting = get_poly(x1 - 158 * alpha, x2, y1, y2)
+        ret[f'meeting'] = meeting
+
+        y1 = y2 + 25 * alpha
+        y2 = y1 + 134 * alpha
+        y1 = y2 + 25 * alpha
+        y2 = y1 + 134 * alpha
+        contact = get_poly(x1, x2, y1, y2)
+        ret[f'contact'] = contact
+
+        y1 = y2 + 25 * alpha
+        y2 = y1 + 134 * alpha
+        train = get_poly(x1, x2, y1, y2)
+        ret[f'train'] = train
+
+        for floor in range(1, 4):
+            x1, y1 = ret[f'dormitory_{floor}'][0]
+            x2, y2 = ret[f'dormitory_{floor}'][2]
+            x2 = x1 - 102 * alpha
+            x1 = x2 - 295 * alpha
+            if floor & 1 == 0:
+                x2 = x1 - 24 * alpha
+                x1 = x2 - 295 * alpha
+            room = get_poly(x1, x2, y1, y2)
+            ret[f'room_{floor}_3'] = room
+            x2 = x1 - 24 * alpha
+            x1 = x2 - 295 * alpha
+            room = get_poly(x1, x2, y1, y2)
+            ret[f'room_{floor}_2'] = room
+            x2 = x1 - 24 * alpha
+            x1 = x2 - 295 * alpha
+            room = get_poly(x1, x2, y1, y2)
+            ret[f'room_{floor}_1'] = room
+
+        if draw:
+            cv2.polylines(im, list(ret.values()), True, (255, 0, 0), 10, cv2.LINE_AA)
             plt.imshow(im)
             plt.show()
 
