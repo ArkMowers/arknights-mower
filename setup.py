91b43c8ffa55d069daaa51ff2c551b35e96a3889
Konano <nanoapezlk@gmail.com>
2021-11-02 20:03:48 +0800

    version 1.3.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     url='https://github.com/Konano/arknights-mower',
     packages=setuptools.find_packages(),
     install_requires=['colorlog', 'opencv_python', 'matplotlib', 'numpy',
-                      'scikit_image', 'scikit_learn', 'onnxruntime', 'pyclipper', 'shapely', 'tornado'],
+                      'scikit_image', 'scikit_learn', 'onnxruntime', 'pyclipper', 'shapely', 'tornado', 'imagehash'],
     include_package_data=True,
     entry_points={'console_scripts': [
         'arknights-mower=arknights_mower.__main__:main']},
