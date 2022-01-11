1d2e4dd75a4d6ce40bb03391438b954434f654b0
YuiTH <396698132@qq.com>
2022-01-11 15:45:13 +0800

    Add pause on executable.

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import os
+import sys
+
 from arknights_mower.__main__ import main
 
 if __name__ == '__main__':
-    main(executable=True)
+    try:
+        main(executable=True)
+    except Exception as e:
+        print(e)
+        print("脚本发生错误")
+        if sys.platform == "win32":
+            os.system("pause")
+        else:
+            input()
+        raise e
+    # Use sys.frozen to check if run through pyinstaller frozen exe, and sys._MEIPASS to get temp path.
