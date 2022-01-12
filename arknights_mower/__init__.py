e683d2d0798348b7a50bc6b58c3152c63df39b2d
YuiTH <396698132@qq.com>
2022-01-12 20:09:43 +0800

    Fix pyinstaller bundle.

--- arknights_mower/__init__.py
+++ arknights_mower/__init__.py
@@ -5,7 +5,8 @@ import sys
 # Use sys.frozen to check if run through pyinstaller frozen exe, and sys._MEIPASS to get temp path.
 if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
     __pyinstall__ = True
-    __rootdir__ = Path(sys._MEIPASS).resolve()
+    # Why they create a  __init__ folder here...idk.
+    __rootdir__ = Path(sys._MEIPASS).joinpath('arknights_mower').joinpath('__init__').resolve()
 else:
     __pyinstall__ = False
     __rootdir__ = Path(__file__).parent.resolve()
@@ -14,4 +15,4 @@ else:
 __cli__ = not (__pyinstall__ and not sys.argv[1:])
 
 __system__ = platform.system().lower()
-__version__ = '1.3.9'
+__version__ = '1.4.2'
