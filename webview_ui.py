771ab34b8b918201a971fdf1a6eebcb4337fb57f
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-05-18 21:58:51 +0800

    使用WebView时，关闭窗口后退出程序

--- webview_ui.py
+++ webview_ui.py
@@ -3,6 +3,8 @@
 import webview
 from server import app
 
+import os
+
 
 def start_server(app):
     app.run(host="127.0.0.1", port="8000")
@@ -10,3 +12,5 @@ def start_server(app):
 
 webview.create_window("Mower", "http://127.0.0.1:8000", width=860, height=600)
 webview.start(start_server, app)
+
+os._exit(0)
