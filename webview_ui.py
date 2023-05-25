988448d6f62dbb5c84e24ca8a2a76d797bbfea83
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-05-25 21:59:49 +0800

    更宽的默认窗口大小

--- webview_ui.py
+++ webview_ui.py
@@ -10,7 +10,7 @@ def start_server(app):
     app.run(host="127.0.0.1", port="8000")
 
 
-webview.create_window("Mower", "http://127.0.0.1:8000", width=860, height=600)
+webview.create_window("Mower", "http://127.0.0.1:8000", width=900, height=600)
 webview.start(start_server, app)
 
 os._exit(0)
