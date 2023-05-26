7ae8fc8738d5aaa4049db518ce3d2a01a7b41805
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-05-26 14:53:59 +0000

    Revert "探索Windows打包"
    This reverts commit 70ef98dd1b204d1d798c4faae7bc825b43fd3af4.

--- webview_ui.py
+++ webview_ui.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-from multiprocessing import freeze_support
+import webview
+from server import app
+
+import os
 
 
 def start_server(app):
@@ -8,12 +11,6 @@ def start_server(app):
 
 
 if __name__ == "__main__":
-    freeze_support()
-
-    import webview
-    from server import app
-    import os
-
     webview.create_window("Mower Web UI in WebView (尚不完善，测试用途，谨慎使用)", "http://127.0.0.1:8000", width=900, height=600)
     webview.start(start_server, app)
 
