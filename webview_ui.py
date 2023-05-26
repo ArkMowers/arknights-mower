77ac7f19e3fb7abd54b22bacb1ee6eb65f7864e4
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-05-26 14:55:46 +0800

    探索Windows打包

--- webview_ui.py
+++ webview_ui.py
@@ -4,6 +4,7 @@ import webview
 from server import app
 
 import os
+import multiprocessing
 
 
 def start_server(app):
@@ -11,7 +12,14 @@ def start_server(app):
 
 
 if __name__ == "__main__":
-    webview.create_window("Mower Web UI in WebView (尚不完善，测试用途，谨慎使用)", "http://127.0.0.1:8000", width=900, height=600)
+    multiprocessing.freeze_support()
+
+    webview.create_window(
+        "Mower Web UI in WebView (尚不完善，测试用途，谨慎使用)",
+        "http://127.0.0.1:8000",
+        width=900,
+        height=600,
+    )
     webview.start(start_server, app)
 
     os._exit(0)
