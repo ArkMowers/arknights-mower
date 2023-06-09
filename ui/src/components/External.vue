c61197f02134338207a0b47cbf9a1d5af28339a1
Jfy <44779230+jiangfeiyang@users.noreply.github.com>
2023-06-09 17:31:05 +0800

    Add files via upload

--- ui/src/components/External.vue
+++ ui/src/components/External.vue
@@ -15,8 +15,15 @@ const {
   maa_path,
   maa_adb_path,
   maa_weekly_plan,
-  maa_rg_enable
+  maa_rg_enable,
+  maa_mall_buy,
+  maa_mall_blacklist,
+  maa_gap,
+  maa_recruitment_time,
+  maa_recruit_only_4
 } = storeToRefs(store)
+
+const { shop_list } = store
 
 async function select_maa_dir() {
   const response = await axios.get(`${import.meta.env.VITE_HTTP_URL}/dialog/folder`)
@@ -77,6 +84,18 @@ async function select_maa_adb_path() {
             </td>
           </tr>
           <tr>
+            <td>MAA启动间隔(小时)：</td>
+            <td>
+              <n-input-number v-model:value="maa_gap"></n-input-number>
+            </td>
+            <td colspan="4">
+              <n-checkbox v-model:checked="maa_recruitment_time">公招三星设置7:40而非9:00</n-checkbox>
+            </td>
+            <td colspan="4">
+              <n-checkbox v-model:checked="maa_recruit_only_4">仅公招四星</n-checkbox>
+            </td>
+          </tr>
+          <tr>
             <td class="table-space">肉鸽：</td>
             <td colspan="3">
               <n-radio-group v-model:value="maa_rg_enable">
@@ -85,6 +104,16 @@ async function select_maa_adb_path() {
                   <n-radio :value="0">禁用</n-radio>
                 </n-space>
               </n-radio-group>
+            </td>
+          </tr>
+          <tr>
+            <td class="table-space maa-mall">信用商店  优先购买</td>
+            <td colspan="2">
+              <n-select multiple filterable tag :options="shop_list" v-model:value="maa_mall_buy" />
+            </td>
+            <td class="table-space maa-mall">信用商店  黑名单</td>
+            <td colspan="2">
+              <n-select multiple filterable tag :options="shop_list" v-model:value="maa_mall_blacklist" />
             </td>
           </tr>
         </table>
@@ -137,5 +166,11 @@ h4 {
 
 .maa-stage {
   min-width: 300px;
+}
+
+.maa-mall {
+  width: 70px;
+  word-wrap: break-word;
+  word-break: break-all;
 }
 </style>
