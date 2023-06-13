3a10d5fcd805d7c2b02aa08312ac1239f99f8c40
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-06-13 20:28:41 +0800

    邮件提醒简单模式

--- ui/src/components/External.vue
+++ ui/src/components/External.vue
@@ -3,13 +3,13 @@ import { useConfigStore } from '@/stores/config'
 import { storeToRefs } from 'pinia'
 import { inject, h } from 'vue'
 import { NTag } from 'naive-ui'
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const axios = inject('axios')
 
 const store = useConfigStore()
 
-const maa_add_task = ref('禁用');
+const maa_add_task = ref('禁用')
 
 const {
   mail_enable,
@@ -36,7 +36,7 @@ const { shop_list } = store
 const sss_option = ref([
   { label: '约翰老妈新建地块', value: 1 },
   { label: '雷神工业测试平台', value: 2 }
-]);
+])
 
 async function select_maa_dir() {
   const response = await axios.get(`${import.meta.env.VITE_HTTP_URL}/dialog/folder`)
@@ -87,29 +87,55 @@ function create_tag(label) {
 }
 
 function selectTab(tab) {
-  maa_add_task.value = tab;
+  maa_add_task.value = tab
 }
 </script>
 
 <template>
-  <div class="home-container">
+  <div class="home-container external-container">
     <n-card>
       <template #header>
-        <n-checkbox v-model:checked="mail_enable">
+        <n-checkbox v-model:checked="mail_enable" class="email-title">
           <div class="card-title">邮件提醒</div>
+          <div class="expand"></div>
+          <n-radio-group class="email-mode">
+            <n-radio-button value="simple" label="简单模式" />
+            <n-radio-button value="advanced" label="高级模式" />
+          </n-radio-group>
         </n-checkbox>
       </template>
       <template #default>
-        <table>
+        <p>在任务完成后发送提醒邮件。</p>
+        <p>
+          在简单模式下，Mower使用您的QQ邮箱。
+          <n-button text tag="a" href="https://service.mail.qq.com/detail/0/75" target="_blank" type="primary">
+            什么是授权码？
+          </n-button>
+        </p>
+        <table class="email-table">
           <tr>
-            <td class="table-space">QQ邮箱</td>
-            <td class="table-space"><n-input v-model:value="account"></n-input></td>
-            <td class="table-space">授权码</td>
+            <td class="email-label">QQ邮箱</td>
+            <td><n-input v-model:value="account"></n-input></td>
+          </tr>
+          <tr>
+            <td class="email-label">授权码</td>
             <td>
               <n-input v-model:value="pass_code" type="password" show-password-on="click"></n-input>
             </td>
           </tr>
         </table>
+        <div class="email-test">
+          <n-button>发送测试邮件</n-button>
+          <div></div>
+        </div>
+        <n-divider />
+        <table class="email-table">
+          <tr>
+            <td class="email-label">邮件主题</td>
+            <td><n-input></n-input></td>
+          </tr>
+        </table>
+        <div>邮件主题可用于区分多开的Mower。</div>
       </template>
     </n-card>
     <n-card>
@@ -140,7 +166,9 @@ function selectTab(tab) {
               <n-input-number v-model:value="maa_gap"></n-input-number>
             </td>
             <td colspan="4">
-              <n-checkbox v-model:checked="maa_recruitment_time">公招三星设置7:40而非9:00</n-checkbox>
+              <n-checkbox v-model:checked="maa_recruitment_time"
+                >公招三星设置7:40而非9:00</n-checkbox
+              >
             </td>
             <td colspan="4">
               <n-checkbox v-model:checked="maa_recruit_only_4">仅公招四星</n-checkbox>
@@ -158,22 +186,36 @@ function selectTab(tab) {
             </td>
           </tr>
           <tr>
-            <td class="table-space maa-mall">信用商店  优先购买</td>
+            <td class="table-space maa-mall">信用商店 优先购买</td>
             <td colspan="2">
               <n-select multiple filterable tag :options="shop_list" v-model:value="maa_mall_buy" />
             </td>
-            <td class="table-space maa-mall">信用商店  黑名单</td>
+            <td class="table-space maa-mall">信用商店 黑名单</td>
             <td colspan="2">
-              <n-select multiple filterable tag :options="shop_list" v-model:value="maa_mall_blacklist" />
+              <n-select
+                multiple
+                filterable
+                tag
+                :options="shop_list"
+                v-model:value="maa_mall_blacklist"
+              />
             </td>
           </tr>
         </table>
-        <div style="border: 1px solid black;width:500px;padding: 10px;">
+        <div style="border: 1px solid black; width: 500px; padding: 10px">
           <div class="tab-buttons">
-            <button @click="selectTab('禁用')" :class="{ 'active': maa_add_task === '禁用' }">禁用</button>
-            <button @click="selectTab('肉鸽')" :class="{ 'active': maa_add_task === '肉鸽' }">肉鸽</button>
-            <button @click="selectTab('保全')" :class="{ 'active': maa_add_task === '保全' }">保全</button>
-            <button @click="selectTab('生息演算')" :class="{ 'active': maa_add_task === '生息演算' }">生息演算</button>
+            <button @click="selectTab('禁用')" :class="{ active: maa_add_task === '禁用' }">
+              禁用
+            </button>
+            <button @click="selectTab('肉鸽')" :class="{ active: maa_add_task === '肉鸽' }">
+              肉鸽
+            </button>
+            <button @click="selectTab('保全')" :class="{ active: maa_add_task === '保全' }">
+              保全
+            </button>
+            <button @click="selectTab('生息演算')" :class="{ active: maa_add_task === '生息演算' }">
+              生息演算
+            </button>
           </div>
 
           <div class="tab-content">
@@ -182,9 +224,13 @@ function selectTab(tab) {
               <table>
                 <tr>
                   <td class="table-space">休息时间开始</td>
-                  <td class="table-space"><n-input v-model:value="sleep_min" placeholder="8:00"></n-input></td>
+                  <td class="table-space">
+                    <n-input v-model:value="sleep_min" placeholder="8:00"></n-input>
+                  </td>
                   <td class="table-space">休息时间结束</td>
-                  <td class="table-space"><n-input v-model:value="sleep_max" placeholder="10:00"></n-input></td>
+                  <td class="table-space">
+                    <n-input v-model:value="sleep_max" placeholder="10:00"></n-input>
+                  </td>
                 </tr>
               </table>
             </div>
@@ -193,16 +239,23 @@ function selectTab(tab) {
                 <tr>
                   <td class="select-label">保全派驻关卡：</td>
                   <td class="table-space">
-                    <n-select tag :options="sss_option" class="sss-select" v-model:value="sss_type" />
+                    <n-select
+                      tag
+                      :options="sss_option"
+                      class="sss-select"
+                      v-model:value="sss_type"
+                    />
                   </td>
                   <td class="select-label">循环次数：</td>
-                  <td style="width: 50px;"><n-input v-model:value="copilot_loop_times" placeholder="10">10</n-input> </td>
-
-
+                  <td style="width: 50px">
+                    <n-input v-model:value="copilot_loop_times" placeholder="10">10</n-input>
+                  </td>
                 </tr>
                 <tr>
                   <td class="select-label">作业地址：</td>
-                  <td colspan="2" class="input-td"><n-input v-model:value="copilot_file_location"></n-input></td>
+                  <td colspan="2" class="input-td">
+                    <n-input v-model:value="copilot_file_location"></n-input>
+                  </td>
                   <td class="table-space">
                     <n-button @click="select_maa_dir">...</n-button>
                   </td>
@@ -236,8 +289,16 @@ function selectTab(tab) {
             </td>
             <td>关卡：</td>
             <td class="table-space maa-stage">
-              <n-select v-model:value="plan.stage" multiple filterable tag :show="false" :show-arrow="false"
-                :render-tag="render_tag" :on-create="create_tag" />
+              <n-select
+                v-model:value="plan.stage"
+                multiple
+                filterable
+                tag
+                :show="false"
+                :show-arrow="false"
+                :render-tag="render_tag"
+                :on-create="create_tag"
+              />
             </td>
             <td>理智药：</td>
             <td>
@@ -251,6 +312,11 @@ function selectTab(tab) {
 </template>
 
 <style scoped>
+.external-container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
 h4 {
   margin: 0;
 }
@@ -280,6 +346,25 @@ h4 {
   margin-top: 16px;
 }
 
+.email-title {
+  width: 100%;
+}
+
+.expand {
+  flex-grow: 1;
+}
+
+.email-table {
+  width: 100%;
+  margin-bottom: 12px;
+}
+
+.email-test {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+
 .card-title {
   font-weight: 500;
   font-size: 16px;
@@ -305,5 +390,30 @@ h4 {
 
 ul {
   padding-left: 24px;
+}
+
+.email-mode {
+  margin-left: 20px;
+}
+
+.email-label {
+  width: 68px;
+}
+
+p {
+  margin: 0 0 10px 0;
+}
+</style>
+
+<style>
+.n-checkbox .n-checkbox__label {
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  padding-right: 0;
+}
+
+.n-divider:not(.n-divider--vertical) {
+  margin: 14px 0 8px;
 }
 </style>
