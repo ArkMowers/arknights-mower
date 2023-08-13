f6b52ff16186d2fda9e59c8869e073b971cde7c6
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-08-13 08:07:47 +0800

    格式化代码

--- ui/src/pages/record.vue
+++ ui/src/pages/record.vue
@@ -1,29 +1,29 @@
 <template>
-    <div>
-        <h1 class="page-title">干员基建报表</h1>
-        <div class="report-switch">
-            <button @click="showMoodReport">干员心情报表</button>
-            <button @click="showWorkRestReport">工作休息比例报表</button>
-        </div>
+  <div>
+    <h1 class="page-title">干员基建报表</h1>
+    <div class="report-switch">
+      <button @click="showMoodReport">干员心情报表</button>
+      <button @click="showWorkRestReport">工作休息比例报表</button>
     </div>
-    <div class="report-container">
-        <div v-for="(groupData, index) in reportData" :key="index" class="report-card">
-            <h2>{{ groupData.groupName }}</h2>
-            <template v-if="currentReport === 'mood'">
-                <Line :data="groupData.moodData" :options="chartOptions" />
-            </template>
-            <template v-else-if="currentReport === 'workRest'">
-                <Pie :data="groupData.workRestData" :options="pieOptions" />
-            </template>
-        </div>
+  </div>
+  <div class="report-container">
+    <div v-for="(groupData, index) in reportData" :key="index" class="report-card">
+      <h2>{{ groupData.groupName }}</h2>
+      <template v-if="currentReport === 'mood'">
+        <Line :data="groupData.moodData" :options="chartOptions" />
+      </template>
+      <template v-else-if="currentReport === 'workRest'">
+        <Pie :data="groupData.workRestData" :options="pieOptions" />
+      </template>
     </div>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { Line,Pie  } from 'vue-chartjs'
+import { Line, Pie } from 'vue-chartjs'
 import 'chartjs-adapter-luxon'
-import ChartDataLabels from 'chartjs-plugin-datalabels';
+import ChartDataLabels from 'chartjs-plugin-datalabels'
 import { useRecordStore } from '@/stores/record'
 import {
   CategoryScale,
@@ -37,7 +37,7 @@ import {
   PointElement,
   Title,
   Tooltip,
-    ArcElement
+  ArcElement
 } from 'chart.js'
 const recordStore = useRecordStore()
 const { getMoodRatios } = recordStore
@@ -53,8 +53,8 @@ ChartJS.register(
   Tooltip,
   Legend,
   Colors,
-    ArcElement,
-    ChartDataLabels
+  ArcElement,
+  ChartDataLabels
 )
 // Mock report data
 const reportData = ref([])
@@ -84,27 +84,26 @@ const chartOptions = ref({
     }
   },
   plugins: {
-      datalabels: {
-          display: false
-      }
+    datalabels: {
+      display: false
+    }
   }
 })
 
 const pieOptions = ref({
-    plugins: {
-        datalabels: {
-            color: 'black',
-            formatter: function(value, context) {
-                let total = context.dataset.data.reduce((sum, currentValue) => sum + currentValue, 0);
-                console.log(value,total,(value/total))
-                return  Math.round((value/total)*100) + '%';
-            }
-        },
-        legend: {
-            // display: false
-        },
-
+  plugins: {
+    datalabels: {
+      color: 'black',
+      formatter: function (value, context) {
+        let total = context.dataset.data.reduce((sum, currentValue) => sum + currentValue, 0)
+        console.log(value, total, value / total)
+        return Math.round((value / total) * 100) + '%'
+      }
+    },
+    legend: {
+      // display: false
     }
+  }
 })
 
 // 添加状态变量来切换报表
@@ -112,11 +111,11 @@ const currentReport = ref('mood') // 默认显示干员心情报表
 
 // 切换报表的方法
 const showMoodReport = () => {
-    currentReport.value = 'mood'
+  currentReport.value = 'mood'
 }
 
 const showWorkRestReport = () => {
-    currentReport.value = 'workRest'
+  currentReport.value = 'workRest'
 }
 </script>
 
@@ -128,15 +127,15 @@ const showWorkRestReport = () => {
 }
 
 .report-card {
-    display: flex;
-    flex-direction: column;
-    align-items: center; /* 让内容在水平方向上居中 */
-    justify-content: center; /* 让内容在垂直方向上居中 */
-    width: 300px;
-    height: 200px;
-    padding: 20px 20px 100px 20px;
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* 让内容在水平方向上居中 */
+  justify-content: center; /* 让内容在垂直方向上居中 */
+  width: 300px;
+  height: 200px;
+  padding: 20px 20px 100px 20px;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 h2 {
@@ -151,26 +150,23 @@ h2 {
   margin-bottom: 20px;
 }
 
-
 /* Style for the switch buttons */
 .report-switch button {
-    padding: 10px 20px;
-    border: none;
-    background-color: #f2f2f2;
-    color: #333;
-    font-size: 16px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
+  padding: 10px 20px;
+  border: none;
+  background-color: #f2f2f2;
+  color: #333;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
 }
 
 .report-switch button:hover {
-    background-color: #ddd;
+  background-color: #ddd;
 }
 
 .report-switch button.active {
-    background-color: #007BFF;
-    color: white;
+  background-color: #007bff;
+  color: white;
 }
-
-
 </style>
