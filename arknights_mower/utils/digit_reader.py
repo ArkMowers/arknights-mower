f79009737a46a9d1c92cb1ee8701ce94358ded09
Bidgecfah <112526130+Bidgecfah@users.noreply.github.com>
2023-09-15 07:55:42 +0800

    支持16:9比例下的多分辨率，识别制造站加速总剩余时间
    1.由原本仅支持1920×1080分辨率改为支持16:9比例下的多分辨率，但是现在的图像识别方案仍然需要一定的清晰度，测试结果是1600×900分辨率能够稳定运行,1600×900分辨率以下会出现识别错误。
    2.识别制造站加速总剩余时间，为切换产物功能做准备

--- arknights_mower/utils/digit_reader.py
+++ arknights_mower/utils/digit_reader.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from pathlib import Path
 import os
-from .image import loadimg
+from .image import loadimg, saveimg
 from .. import __rootdir__
 
 
@@ -22,8 +22,9 @@ class DigitReader:
                 loadimg(f'{__rootdir__}/resources/drone_count/{i}.png', True)
             )
 
-    def get_drone(self, img_grey):
-        drone_part = img_grey[32:76, 1144:1225]
+    def get_drone(self, img_grey, h, w):
+        drone_part = img_grey[h * 32 // 1080:h * 76 // 1080, w * 1144 // 1920:w * 1225 // 1920]
+        drone_part = cv.resize(drone_part, (81, 44), interpolation=cv.INTER_AREA)
         result = {}
         for j in range(10):
             res = cv.matchTemplate(
@@ -45,8 +46,9 @@ class DigitReader:
         l = [str(result[k]) for k in sorted(result)]
         return int("".join(l))
 
-    def get_time(self, img_grey):
-        digit_part = img_grey[510:543, 499:1920]
+    def get_time(self, img_grey, h, w):
+        digit_part = img_grey[h * 510 // 1080:h * 543 // 1080, w * 499 // 1920:w]
+        digit_part = cv.resize(digit_part, (1421, 33), interpolation=cv.INTER_AREA)
         result = {}
         for j in range(10):
             res = cv.matchTemplate(
@@ -65,10 +67,34 @@ class DigitReader:
                         break
                 if accept:
                     if len(result) == 0:
-                        digit_part = digit_part[:, loc[1][i] - 5 : loc[1][i] + 116]
+                        digit_part = digit_part[:, loc[1][i] - 5: loc[1][i] + 116]
                         offset = loc[1][0] - 5
                         for m in range(len(loc[1])):
                             loc[1][m] -= offset
                     result[loc[1][i]] = j
         l = [str(result[k]) for k in sorted(result)]
         return f"{l[0]}{l[1]}:{l[2]}{l[3]}:{l[4]}{l[5]}"
+
+    def 识别制造加速总剩余时间(self, img_grey, h, w):
+        时间部分 = img_grey[h * 665 // 1080:h * 709 // 1080, w * 750 // 1920:w * 960 // 1920]
+        时间部分 = cv.resize(时间部分, (210*58//71, 44*58//71), interpolation=cv.INTER_AREA)
+        result = {}
+        for j in range(10):
+            res = cv.matchTemplate(
+                时间部分,
+                self.drone_template[j],
+                cv.TM_CCORR_NORMED,
+            )
+            threshold = 0.92
+            loc = np.where(res >= threshold)
+            for i in range(len(loc[0])):
+                offset = loc[1][i]
+                accept = True
+                for o in result:
+                    if abs(o - offset) < 5:
+                        accept = False
+                        break
+                if accept:
+                    result[loc[1][i]] = j
+        l = [str(result[k]) for k in sorted(result)]
+        return f"{l[0]}{l[1]}{l[2]}:{l[3]}{l[4]}:{l[5]}{l[6]}"
