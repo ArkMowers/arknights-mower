3e18f2f8d636a0cac1c5e9f28265a3a70c19df5a
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-10-01 09:35:19 +0800

    修复paddle_recog评分

--- arknights_mower/utils/character_recognize.py
+++ arknights_mower/utils/character_recognize.py
@@ -145,11 +145,11 @@ def paddle_recog(__img):
         for x in agent_sorted:
             score = -abs(len(x) - len(recog_text))
             for c in set(x):
-                score += 2 if c in recog_text else 0
+                score += 3 if c in recog_text else 0
             if score > best_score:
                 best = x
                 best_score = score
-        if best_score > len(best):
+        if best_score > len(best) * 2:
             logger.debug(f"{recog_text} --?--> {best}")
             return best
     return None
