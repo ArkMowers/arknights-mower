3a727815d5738dbbf164db3300b3b198fbae0413
EightyDollars <EightyDollars@163.com>
2023-11-09 05:40:09 +0800

    add:requirements.txt增加pandas模块

--- arknights_mower/__main__.py
+++ arknights_mower/__main__.py
@@ -343,7 +343,7 @@ def simulate():
                     # 刷新时间以鹰历为准
                     if base_scheduler.daily_mission != (datetime.now() - timedelta(hours=4)).date():
                         base_scheduler.daily_mission = (datetime.now() - timedelta(hours=4)).date()
-                        #base_scheduler.mail_plan_solver()
+                        base_scheduler.mail_plan_solver()
                         daily_report()
                     if base_scheduler.skland_config["skland_enable"] == 1:
                         base_scheduler.skland_plan_solover()
