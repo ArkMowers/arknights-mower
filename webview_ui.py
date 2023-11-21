4e06394e711868389c76372f45f0ddc2499e0f97
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-11-21 23:55:08 +0800

    尝试设置端口检测超时时间

--- webview_ui.py
+++ webview_ui.py
@@ -115,6 +115,7 @@ if __name__ == "__main__":
 
     def is_port_in_use(port):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(0.1)
             return s.connect_ex(("localhost", port)) == 0
 
     if is_port_in_use(port):
