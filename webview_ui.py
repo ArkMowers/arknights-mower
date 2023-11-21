7c5c372ed330db7e0b8db2dfdd0f0fe566152601
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-11-21 16:16:04 +0800

    优化启动流程

--- webview_ui.py
+++ webview_ui.py
@@ -1,106 +1,95 @@
 #!/usr/bin/env python3
-
-import webview
-import server
-from server import app
-
 import multiprocessing
-
-from arknights_mower.utils.conf import load_conf, save_conf
-from arknights_mower.utils.path import get_path
-from arknights_mower.__init__ import __version__
-
-from threading import Thread
-from PIL import Image, ImageTk
-from pystray import Icon, Menu, MenuItem
-
-import socket
-import tkinter as tk
-from tkinter.font import Font
-from tkinter import messagebox
-from time import sleep
-import sys
-
-
-quit_app = False
-display = True
-window = None
-
-
-def on_resized(w, h):
-    global width
-    global height
-
-    width = w
-    height = h
-
-
-def toggle_window():
-    global window
-    global display
-    window.hide() if display else window.show()
-    display = not display
-
-
-def on_closing():
-    if quit_app:
-        return True
-    Thread(target=toggle_window).start()
-    return False
-
-
-def destroy_window():
-    global quit_app
-    global window
-    quit_app = True
-    window.destroy()
-
-
-def is_port_in_use(port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(("localhost", port)) == 0
-
-
-logo_path = get_path("@internal/logo.png")
-tray_img = Image.open(logo_path)
-
-
-class SplashScreen:
-    def __init__(self):
-        self.root = tk.Tk()
-
-        self.canvas = tk.Canvas(self.root, width=256, height=256)
-        self.canvas.pack()
-        self.img = ImageTk.PhotoImage(tray_img)
-        self.canvas.create_image(128, 128, image=self.img)
-
-        self.title_font = Font(size=24)
-        self.title_label = tk.Label(
-            self.root, text=f"arknights-mower {__version__}", font=self.title_font
-        )
-        self.title_label.pack()
-
-        self.loading_label = tk.Label(self.root)
-        self.loading_label.pack()
-        self.root.overrideredirect(True)
-
-    def show_text(self, text):
-        self.loading_label.config(text=text)
-        self.center()
-
-    def center(self):
-        self.root.eval("tk::PlaceWindow . center")
-        self.root.update()
-
-    def stop(self):
-        self.root.destroy()
 
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
 
+    import tkinter as tk
+    from tkinter.font import Font
+    from arknights_mower.__init__ import __version__
+
+    class SplashScreen:
+        def __init__(self):
+            self.root = tk.Tk()
+
+            self.canvas = tk.Canvas(self.root, width=256, height=256)
+            self.canvas.pack()
+
+            self.title_font = Font(size=24)
+            self.title_label = tk.Label(
+                self.root, text=f"arknights-mower {__version__}", font=self.title_font
+            )
+            self.title_label.pack()
+
+            self.loading_label = tk.Label(self.root)
+            self.loading_label.pack()
+            self.root.overrideredirect(True)
+
+        def load_img(self, img):
+            self.img = ImageTk.PhotoImage(img)
+            self.canvas.create_image(128, 128, image=self.img)
+            self.root.update()
+
+        def show_text(self, text):
+            self.loading_label.config(text=text + "……")
+            self.center()
+
+        def center(self):
+            self.root.eval("tk::PlaceWindow . center")
+            self.root.update()
+
+        def hide(self):
+            self.root.withdraw()
+
+        def stop(self):
+            self.root.destroy()
+
     splash = SplashScreen()
+
+    splash.show_text("加载图标")
+
+    from PIL import Image, ImageTk
+    from arknights_mower.utils.path import get_path
+
+    logo_path = get_path("@internal/logo.png")
+    tray_img = Image.open(logo_path)
+    splash.load_img(tray_img)
+
+    splash.show_text("加载依赖")
+
+    quit_app = False
+    display = True
+    window = None
+
+    def on_resized(w, h):
+        global width
+        global height
+
+        width = w
+        height = h
+
+    def toggle_window():
+        global window
+        global display
+        window.hide() if display else window.show()
+        display = not display
+
+    def on_closing():
+        if quit_app:
+            return True
+        Thread(target=toggle_window).start()
+        return False
+
+    def destroy_window():
+        global quit_app
+        global window
+        quit_app = True
+        window.destroy()
+
     splash.show_text("加载配置文件")
+
+    from arknights_mower.utils.conf import load_conf, save_conf
 
     conf = load_conf()
 
@@ -108,14 +97,40 @@ if __name__ == "__main__":
     token = conf["webview"]["token"]
     host = "0.0.0.0" if token else "127.0.0.1"
 
+    global width
+    global height
+
+    width = conf["webview"]["width"]
+    height = conf["webview"]["height"]
+
+    splash.show_text("检测端口占用")
+
+    import socket
+
+    def is_port_in_use(port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return s.connect_ex(("localhost", port)) == 0
+
+    from tkinter import messagebox
+    import sys
+
     if is_port_in_use(port):
+        splash.hide()
         messagebox.showerror(
             "arknights-mower",
             f"端口{port}已被占用，无法启动！",
         )
+        splash.stop()
         sys.exit()
 
-    splash.show_text("启动flask网页服务器")
+    splash.show_text("加载Flask依赖")
+
+    import server
+    from server import app
+
+    splash.show_text("启动Flask网页服务器")
+
+    from threading import Thread
 
     app.token = token
     Thread(
@@ -124,13 +139,9 @@ if __name__ == "__main__":
         daemon=True,
     ).start()
 
-    global width
-    global height
-
-    width = conf["webview"]["width"]
-    height = conf["webview"]["height"]
-
     splash.show_text("加载托盘图标")
+
+    from pystray import Icon, Menu, MenuItem
 
     icon = Icon(
         "arknights-mower",
@@ -151,6 +162,8 @@ if __name__ == "__main__":
 
     splash.show_text("准备主窗口")
 
+    import webview
+
     window = webview.create_window(
         f"arknights-mower {__version__} (http://{host}:{port})",
         f"http://127.0.0.1:{port}?token={token}",
@@ -161,6 +174,8 @@ if __name__ == "__main__":
 
     window.events.resized += on_resized
     window.events.closing += on_closing
+
+    from time import sleep
 
     while not is_port_in_use(port):
         sleep(0.1)
