2ebef464fe1ca94162adeee89ae6bcd9e4a9946a
EightyDollars <EightyDollars@163.com>
2023-11-22 19:46:05 +0800

    fix:招募四星以上干员时产生异常无法继续公招

--- arknights_mower/solvers/recruit.py
+++ arknights_mower/solvers/recruit.py
@@ -84,7 +84,8 @@ class RecruitSolver(BaseSolver):
                 agent = []
                 for item in self.agent_choose[pos]['result']:
                     agent.append(item['name'])
-                logger.info("{}:[".format(pos) + ",".join(self.agent_choose[pos]['tags']) + "]:{}".format(",".join(agent)))
+                logger.info(
+                    "{}:[".format(pos) + ",".join(self.agent_choose[pos]['tags']) + "]:{}".format(",".join(agent)))
         if self.agent_choose or self.result_agent:
             self.send_message(recruit_template.render(recruit_results=self.agent_choose,
                                                       recruit_get_agent=self.result_agent,
@@ -105,7 +106,9 @@ class RecruitSolver(BaseSolver):
         self.recruit_config = {
             "recruitment_time": {
                 "3": recruitment_time,
-                "4": 540
+                "4": 540,
+                "5": 540,
+                "6": 540
             },
             "recruit_robot": recruit_config['recruit_robot'],
             "permit_target": recruit_config['permit_target']
@@ -291,6 +294,7 @@ class RecruitSolver(BaseSolver):
                 self.device.tap((left + x[2][0][0] - 5, up + x[2][0][1] - 5))
 
         # 9h为True 3h50min为False
+        logger.debug("开始选择时长")
         recruit_time_choose = self.recruit_config["recruitment_time"]["3"]
         if recruit_result_level >= 3:
             if recruit_result_level == 1:
