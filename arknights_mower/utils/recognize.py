e8c14ed8db721f63537b583eab0f5c09236e256f
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2024-02-20 09:36:47 +0800

    生息演算修复退出战斗识别

--- arknights_mower/utils/recognize.py
+++ arknights_mower/utils/recognize.py
@@ -305,7 +305,7 @@ class Recognizer(object):
         return self.scene
 
     def find_ra_battle_exit(self) -> bool:
-        im = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+        im = cv2.cvtColor(self.img, cv2.COLOR_RGB2HSV)
         im = cv2.inRange(im, (29, 0, 0), (31, 255, 255))
         score, scope = self.template_match("ra/battle_exit", ((75, 47), (165, 126)), cv2.TM_CCOEFF_NORMED)
         return scope if score > 0.8 else None
