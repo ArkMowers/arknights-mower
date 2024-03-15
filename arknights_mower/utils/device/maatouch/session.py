a9be7fbda76fa9d6aee43f7c02c298af4c7f98c1
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2024-03-15 22:32:24 +0800

    调用MaaTouch时指定adb设备

--- arknights_mower/utils/device/maatouch/session.py
+++ arknights_mower/utils/device/maatouch/session.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import platform
 import subprocess
-import traceback
 
 from arknights_mower.utils.device.adb_client import ADBClient
 
@@ -11,41 +10,37 @@ from ...log import logger
 
 class Session(object):
     def __init__(self, client: ADBClient) -> None:
-        try:
-            self.process = subprocess.Popen(
-                [
-                    client.adb_bin,
-                    "shell",
-                    "CLASSPATH=/data/local/tmp/maatouch",
-                    "app_process",
-                    "/",
-                    "com.shxyke.MaaTouch.App",
-                ],
-                stdout=subprocess.PIPE,
-                stdin=subprocess.PIPE,
-                text=True,
-                creationflags=subprocess.CREATE_NO_WINDOW
-                if platform.system() == "Windows"
-                else 0,
-            )
-            output_line = self.process.stdout.readline()
-            logger.debug("maatouch output line 1: " + output_line.replace("\n", "\\n"))
-            # ^ <max-contacts> <max-x> <max-y> <max-pressure>
-            _, max_contacts, max_x, max_y, max_pressure, *_ = output_line.strip().split(
-                " "
-            )
-            self.max_contacts = max_contacts
-            self.max_x = max_x
-            self.max_y = max_y
-            self.max_pressure = max_pressure
+        self.process = subprocess.Popen(
+            [
+                client.adb_bin,
+                "-s",
+                client.device_id,
+                "shell",
+                "CLASSPATH=/data/local/tmp/maatouch",
+                "app_process",
+                "/",
+                "com.shxyke.MaaTouch.App",
+            ],
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            text=True,
+            creationflags=subprocess.CREATE_NO_WINDOW
+            if platform.system() == "Windows"
+            else 0,
+        )
 
-            output_line = self.process.stdout.readline()
-            logger.debug("maatouch output line 2: " + output_line.replace("\n", "\\n"))
-            # $ <pid>
-            _, pid = output_line.strip().split(" ")
-            self.pid = pid
-        except Exception:
-            logger.error(traceback.format_exc())
+        # ^ <max-contacts> <max-x> <max-y> <max-pressure>
+        _, max_contacts, max_x, max_y, max_pressure, *_ = (
+            self.process.stdout.readline().strip().split(" ")
+        )
+        self.max_contacts = max_contacts
+        self.max_x = max_x
+        self.max_y = max_y
+        self.max_pressure = max_pressure
+
+        # $ <pid>
+        _, pid = self.process.stdout.readline().strip().split(" ")
+        self.pid = pid
 
         logger.debug(f"maatouch running, pid: {self.pid}")
         logger.debug(
