e0e33a8970d46da1f9a062e74b6eb84532f90e7f
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2024-03-15 15:10:47 +0800

    调用MaaTouch出错时向日志打印错误信息

--- arknights_mower/utils/device/maatouch/session.py
+++ arknights_mower/utils/device/maatouch/session.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import platform
 import subprocess
+import traceback
 
 from arknights_mower.utils.device.adb_client import ADBClient
 
@@ -10,39 +11,38 @@ from ...log import logger
 
 class Session(object):
     def __init__(self, client: ADBClient) -> None:
-        self.process = subprocess.Popen(
-            [
-                client.adb_bin,
-                "shell",
-                "CLASSPATH=/data/local/tmp/maatouch",
-                "app_process",
-                "/",
-                "com.shxyke.MaaTouch.App",
-            ],
-            stdout=subprocess.PIPE,
-            stdin=subprocess.PIPE,
-            text=True,
-            bufsize=0,
-            creationflags=subprocess.CREATE_NO_WINDOW
-            if platform.system() == "Windows"
-            else 0,
-        )
-
         try:
+            self.process = subprocess.Popen(
+                [
+                    client.adb_bin,
+                    "shell",
+                    "CLASSPATH=/data/local/tmp/maatouch",
+                    "app_process",
+                    "/",
+                    "com.shxyke.MaaTouch.App",
+                ],
+                stdout=subprocess.PIPE,
+                stdin=subprocess.PIPE,
+                text=True,
+                bufsize=0,
+                creationflags=subprocess.CREATE_NO_WINDOW
+                if platform.system() == "Windows"
+                else 0,
+            )
             # ^ <max-contacts> <max-x> <max-y> <max-pressure>
             _, max_contacts, max_x, max_y, max_pressure, *_ = (
                 self.process.stdout.readline().strip().split(" ")
             )
-        except Exception as e:
-            print(e)
-        self.max_contacts = max_contacts
-        self.max_x = max_x
-        self.max_y = max_y
-        self.max_pressure = max_pressure
+            self.max_contacts = max_contacts
+            self.max_x = max_x
+            self.max_y = max_y
+            self.max_pressure = max_pressure
 
-        # $ <pid>
-        _, pid = self.process.stdout.readline().strip().split(" ")
-        self.pid = pid
+            # $ <pid>
+            _, pid = self.process.stdout.readline().strip().split(" ")
+            self.pid = pid
+        except Exception:
+            logger.error(traceback.format_exc())
 
         logger.debug(f"maatouch running, pid: {self.pid}")
         logger.debug(
