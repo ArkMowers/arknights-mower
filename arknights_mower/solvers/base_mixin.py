164a29f7b74c952c1cbdb9e5ac69b4dd2d94696f
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2024-03-21 13:51:44 +0800

    产物收取识别限制区域

--- arknights_mower/solvers/base_mixin.py
+++ arknights_mower/solvers/base_mixin.py
@@ -206,7 +206,7 @@ class BaseMixin:
         
     def detect_product_complete(self):
         for product in ["gold", "exp", "lmd", "ori"]:
-            if pos := self.find(f"infra_{product}_complete", score=0.1):
+            if pos := self.find(f"infra_{product}_complete", score=0.1, scope=((1230, 0), (1920, 1080))):
                 return pos
 
     def read_operator_in_room(self, img):
