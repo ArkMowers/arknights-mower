2f362e467744f03a94af8f24b4f38463fed54ee8
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2024-04-28 08:59:11 +0800

    月卡签到活动改进

--- arknights_mower/solvers/sign_in.py
+++ arknights_mower/solvers/sign_in.py
@@ -6,43 +6,45 @@ class SignInSolver(BaseSolver):
     def run(self) -> None:
         logger.info("Start: 签到活动")
         self.back_to_index()
+        self.materiel = False
         super().run()
+
+    def notify(self, msg):
+        logger.info(msg)
+        self.recog.save_screencap("sign_in")
+        if hasattr(self, "send_message_config") and self.send_message_config:
+            self.send_message(msg)
 
     def transition(self) -> bool:
         failure = 0
 
-        if self.recog.detect_connecting_scene():
-            self.sleep()
-        elif self.recog.detect_index_scene():
+        if self.recog.detect_index_scene():
+            if self.materiel:
+                return True
             if pos := self.find("sign_in/entry"):
-                self.sleep()
-                orange_dot = (255, 104, 1)
-                x, y = pos[0]
-                x += 238
-                y += 14
-                if all(self.get_color((x, y)) == orange_dot):
-                    self.tap(pos)
-                else:
-                    msg = "五周年专享月卡已领取"
-                    logger.info(msg)
-                    self.send_message(msg)
-                    return True
-            else:
-                msg = "未检测到五周年月卡领取入口！"
-                logger.info(msg)
-                self.send_message(msg)
-        elif self.find("sign_in/banner"):
-            if pos := self.find("sign_in/button_ok"):
                 self.tap(pos)
             else:
+                self.notify("未检测到五周年月卡领取入口！")
+                return True
+        elif self.find("sign_in/banner"):
+            if self.materiel:
                 self.back()
+                return
+            if pos := self.find("sign_in/button_ok"):
+                self.tap(pos)
+                return
+            self.materiel = True
+            self.notify("今天的五周年专享月卡已经领取过了")
+            self.back()
         elif self.find("materiel_ico", scope=((860, 60), (1072, 217))):
+            if not self.materiel:
+                self.notify("成功领取五周年专享月卡")
+                self.materiel = True
             self.tap((960, 960))
         else:
             failure += 1
             if failure > 15:
-                msg = "签到任务执行失败！"
-                logger.info(msg)
-                self.send_message(msg)
+                self.notify("签到任务执行失败！")
+                self.back_to_index()
                 return True
             self.sleep()
