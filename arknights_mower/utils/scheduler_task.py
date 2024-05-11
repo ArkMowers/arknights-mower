b3d28d3bea20c6759598141268af89327849ca6a
Shawnsdaddy <wu2xx@dukes.jmu.edu>
2024-05-11 16:12:23 -0700

    #550 新增强制刷新跑单任务

--- arknights_mower/utils/scheduler_task.py
+++ arknights_mower/utils/scheduler_task.py
@@ -21,6 +21,7 @@ class TaskTypes(Enum):
     SKLAND = ("skland", "森空岛签到", 2)
     RE_ORDER = ("宿舍排序", "宿舍排序", 2)
     RELEASE_DORM = ("释放宿舍空位", "释放宿舍空位", 2)
+    REFRESH_ORDER_TIME = ("强制刷新跑单时间", "刷新跑单时间", 2)
     DEPOT = ("仓库扫描", "仓库扫描", 2)  # 但是我不会写剩下的
 
     def __new__(cls, value, display_value, priority):
