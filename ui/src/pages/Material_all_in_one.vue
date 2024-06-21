f801b64a44d1dc99ea01329c1061e90f1e39c1ca
fuyn101 <fuynshile@outlook.com>
2024-06-21 11:59:07 +0800

    碳条改成sk5/删掉一些用不到的前端页面

--- ui/src/pages/Material_all_in_one.vue
+++ ui/src/pages/Material_all_in_one.vue
@@ -1,71 +1,60 @@
-<script setup>
-import { ref, onMounted, watch } from 'vue'
-import { SlickList, SlickItem } from 'vue-slicksort'
-import event_json from './stage_data/event_data.json'
-const event_list = ref([])
-event_list.value = event_json
-
-watch(
-  () => event_list.value,
-  (newValue, oldValue) => {
-    event_list.value = event_json
-  }
-)
-
-const weekly_result = ref({
-  星期1: ref([]),
-  星期2: ref([]),
-  星期3: ref([]),
-  星期4: ref([]),
-  星期5: ref([]),
-  星期6: ref([]),
-  星期7: ref([])
-})
-const deleteResult = (key) => {
-  weekly_result.value[key] = []
-}
-</script>
 <template>
-  <n-flex>
-    <div>
-      <n-thing>
-        <n-button>关卡</n-button>
-        <SlickList axis="y" v-model:list="event_list" group="123">
-          <SlickItem v-for="(event, i) in event_list" :key="event" :index="i">
-            <n-list bordered hoverable clickable>
-              <n-list-item> {{ event['id'] }} {{ event['name'] }} </n-list-item>
-            </n-list>
-          </SlickItem>
-        </SlickList>
-      </n-thing>
-    </div>
-
-    <div v-for="(result, day) in weekly_result" :key="day">
-      <n-thing class="card_out">
-        <n-button @click="deleteResult(day)">{{ day }} 重选</n-button>
-        <SlickList axis="y" v-model:list="weekly_result[day]" group="123">
-          <SlickItem v-for="(event, i) in weekly_result[day]" :key="event" :index="i">
-            <n-list bordered hoverable clickable>
-              <n-list-item> {{ event['id'] }} {{ event['name'] }} </n-list-item>
-            </n-list>
-          </SlickItem>
-        </SlickList>
-        {{ weekly_result[day] }}
-      </n-thing>
-    </div>
-  </n-flex>
+  <n-data-table
+    :columns="columns"
+    :data="data"
+    :pagination="pagination"
+    :bordered="false"
+    style="width: 40%"
+  />
 </template>
-<style>
-.unselectable {
-  user-select: none;
-  /* Standard syntax */
-}
 
-.card_out {
-  width: 150px;
-}
+<script setup>
+import { h } from 'vue'
+import { NButton, useMessage } from 'naive-ui'
+import event_data1 from '@/pages/stage_data/event_data.json'
 
-.card_in {
-  width: 100px;
-}
-</style>
+const createColumns = () => [
+  {
+    title: 'Name',
+    key: 'name'
+  },
+  {
+    title: 'ID',
+    key: 'id'
+  },
+  {
+    title: '周一',
+    key: '周一'
+  },
+  {
+    title: '周二',
+    key: '周二'
+  },
+  {
+    title: '周三',
+    key: '周三'
+  },
+  {
+    title: '周四',
+    key: '周四'
+  },
+  {
+    title: '周五',
+    key: '周五'
+  },
+  {
+    title: '周六',
+    key: '周六'
+  },
+  {
+    title: '周日',
+    key: '周日'
+  }
+]
+
+const columns = createColumns()
+
+const pagination = false
+
+const data = event_data1
+</script>
