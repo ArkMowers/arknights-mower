08f07f8250686f0d04426e1d9f4cdd3d6635eee1
fuyn101 <fuynshile@outlook.com>
2024-06-27 23:09:12 +0800

    基建技能页面优化+存储数据优化

---
+++ ui/src/components/bufferinfo.vue
@@ -0,0 +1,49 @@
+<template>
+  <span v-html="des" @click="showModal = true"></span>
+  <n-modal v-model:show="showModal">
+    <n-card
+      style="width: 600px"
+      title="特殊效果"
+      :bordered="false"
+      size="huge"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div v-for="item in props.buffer">
+        <n-card :title="buffer1[item].termName">
+          <div v-html="richText2HTML(buffer1[item].description)"></div>
+        </n-card>
+      </div>
+    </n-card>
+  </n-modal>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import { richText2HTML } from '@/stores/richText2HTML'
+import buffer1 from '@/pages/buffer.json'
+const showModal = ref(false)
+const props = defineProps({
+  des: String,
+  isbuffer: Boolean,
+  buffer: Array
+})
+</script>
+
+<style>
+.cc-vup {
+  color: #0098dc;
+}
+
+.cc-vdown {
+  color: #ff6237;
+}
+
+.cc-rem {
+  color: #f49800;
+}
+
+.cc-kw {
+  color: #00b0ff;
+}
+</style>
