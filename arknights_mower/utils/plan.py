0acb94f180f6b9f1547a3b09a41150e75c58e378
Anyk00 <132000824+Anyk00@users.noreply.github.com>
2024-09-26 15:24:55 +0800

    取消副表标题传入后端

--- arknights_mower/utils/plan.py
+++ arknights_mower/utils/plan.py
@@ -150,14 +150,12 @@ class Plan:
             trigger: 触发备用plan 的条件（必填）就是每次最多只有一个备用plan触发
             task: 触发备用plan 的时间生成的任务（选填）
             trigger_timing: 触发时机
-            name: plan标题
         """
         self.plan = plan
         self.config = config
         self.trigger = trigger
         self.task = task
         self.trigger_timing = self.set_timing_enum(trigger_timing)
-        self.name = name
 
     @staticmethod
     def set_timing_enum(value: str) -> PlanTriggerTiming:
