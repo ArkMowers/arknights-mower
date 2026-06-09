fd4de6b8b41dd4668c34438ef7296ed43c7266b0
Shawnsdaddy <franky.wu2333@gmail.com>
2026-06-08 20:06:08 -0700

    test: add test-arrange CLI command

--- arknights_mower/scheduler/__main__.py
+++ arknights_mower/scheduler/__main__.py
@@ -25,5 +25,8 @@ def test_scene():
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "test-scene":
         test_scene()
+    elif len(sys.argv) > 1 and sys.argv[1] == "test-arrange":
+        from arknights_mower.scheduler.bootstrap import run as _run
+        _run(start_type="test")
     else:
         run()
