c4b82cce6773b66acc78660724f542ddb7b675cf
ALEXsun0 <96333324+ALEXsun0@users.noreply.github.com>
2026-09-26 03:19:16 +0800

    fix(ui): 移除房间位置选项中的截图提示

--- ui/src/pages/Settings.vue
+++ ui/src/pages/Settings.vue
@@ -758,7 +758,7 @@ if (return_home_when_idle.value) {
           >
             <n-form-item label="右侧房间位置">
               <n-checkbox v-model:checked="swap_contact_train">
-                训练室在办公室上方（如截图）
+                训练室在办公室上方
               </n-checkbox>
             </n-form-item>
             <n-form-item>
